92b04d492ec741cc1edff86ccc0e7410a877b6eb
Harsh Jindal <93310969+harshjindall@users.noreply.github.com>
2025-08-14 15:56:23 +0530

    Update FloatingSocialIcons.js

--- src/MyComponents/FloatingSocialIcons.js
+++ src/MyComponents/FloatingSocialIcons.js
@@ -6,27 +6,27 @@ const FloatingSocialIcons = () => {
   return (
     <div className="floating-social-icons">
       <ImageButton 
-        imageSrc={process.env.PUBLIC_URL + "/githublogo.png"}
+        imageSrc={process.env.PUBLIC_URL + "/githublogo.webp"}
         altText="GitHub"
         onClick={() => window.open("https://github.com/HarshJindall", "_blank")}
       />
       <ImageButton 
-        imageSrc={process.env.PUBLIC_URL + "/devpostlogo.png"}
+        imageSrc={process.env.PUBLIC_URL + "/devpostlogo.webp"}
         altText="Devpost"
         onClick={() => window.open("https://devpost.com/harshjindal", "_blank")}
       />
       <ImageButton 
-        imageSrc={process.env.PUBLIC_URL + "/youtubelogo.png"}
+        imageSrc={process.env.PUBLIC_URL + "/youtubelogo.webp"}
         altText="YouTube"
         onClick={() => window.open("https://www.youtube.com/@HarshJindall", "_blank")}
       />
       <ImageButton 
-        imageSrc={process.env.PUBLIC_URL + "/instagramlogo.png"}
+        imageSrc={process.env.PUBLIC_URL + "/instagramlogo.webp"}
         altText="Instagram"
         onClick={() => window.open("https://www.instagram.com/HarshJindall/", "_blank")}
       />
       <ImageButton 
-        imageSrc={process.env.PUBLIC_URL + "/linkedinlogo.png"}
+        imageSrc={process.env.PUBLIC_URL + "/linkedinlogo.webp"}
         altText="LinkedIn"
         onClick={() => window.open("https://www.linkedin.com/in/HarshJindal/", "_blank")}
       />
