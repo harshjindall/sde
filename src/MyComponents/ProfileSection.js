27669626a8833844218db9fcee7b37397e869d14
Harsh Jindal <93310969+harshjindall@users.noreply.github.com>
2025-08-15 15:28:43 +0530

    Update ProfileSection.js

--- src/MyComponents/ProfileSection.js
+++ src/MyComponents/ProfileSection.js
@@ -88,7 +88,7 @@ const ProfileSection = () => {
               Trader / Engineer / Builder
             </h2>
             <p className="spaceStyleText"> 
-              22 y/o Trader & SWE working on building the next gen AI startup. Currently working @ Jindal Marketing. <span className="subtle-emphasis">I am leveraging code and data to drive trading strategies. 3x University-Level Competition Winner. Passionate about algorithmic trading, predictive modeling, AI systems, and startups. Trading enthusiast with eyes on launching my own quants firm.<br>Always building, Always Learning.</span>
+              22 y/o Trader & SWE working on building the next gen AI startup. Currently working @ Jindal Marketing. <span className="subtle-emphasis">I am leveraging code and data to drive trading strategies. 3x University-Level Competition Winner. Passionate about algorithmic trading, predictive modeling, AI systems, and startups. Trading enthusiast with eyes on launching my own quants firm.<br />Always building, Always Learning.</span>
             </p>
           </div>
         </div>
